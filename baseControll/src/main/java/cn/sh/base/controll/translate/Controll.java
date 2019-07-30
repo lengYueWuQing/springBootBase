7173380c29ad36eb98b6dd489f7a4b040c757464
lengYueWuQing <49423403+lengYueWuQing@users.noreply.github.com>
2019-07-30 13:15:00 +0800

    页面动态修改
    测试后台编辑html页面后传到前台，达到动态编辑的结果

--- baseControll/src/main/java/cn/sh/base/controll/translate/Controll.java
+++ baseControll/src/main/java/cn/sh/base/controll/translate/Controll.java
@@ -1,7 +1,17 @@
 package cn.sh.base.controll.translate;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.nio.file.Files;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +24,7 @@ import cn.sh.base.service.translate.ResponseUtils;
 import cn.sh.base.service.translate.Translate;
 import cn.sh.base.service.translate.UpdateXml;
 import cn.sh.base.service.translate.UploadAnalyze;
+
 @RestController
 public class Controll {
 	
@@ -51,6 +62,40 @@ public class Controll {
 	public void updateXml(HttpServletRequest request, HttpServletResponse response){
 		try {
 			updateXml.run(request, response);
+		} catch (Exception e) {
+			ResponseUtils.setErrorMessage(response, "保存内容失败");
+			LOG.error("", e);
+		}
+	}
+	
+	/**
+	   * 页面修改测试
+	 * @param request
+	 * @param response
+	 */
+	@RequestMapping(path = "/json/test", method = RequestMethod.GET)
+	public void htmlTest(HttpServletRequest request, HttpServletResponse response){
+		try {
+			response.setContentType("text/html;charset=utf-8");
+			InputStream in = this.getClass().getResourceAsStream("/test.html");
+			if(in == null) {
+				return;
+			}
+			StringBuilder datas = new StringBuilder();
+			BufferedInputStream buff = new BufferedInputStream(in);
+			byte[] byt = new byte[1024];
+			while((buff.read(byt))!= -1) {
+				datas.append(new String(byt, "utf-8"));
+			}
+			Document document = Jsoup.parse(datas.toString(), "utf-8");
+			Element element = document.getElementById("test");
+			if(element != null) {
+				Elements elements = element.children();
+				for(Element el : elements) {
+					el.html("成功");
+				}
+			}
+			response.getWriter().print(document.html());
 		} catch (Exception e) {
 			ResponseUtils.setErrorMessage(response, "保存内容失败");
 			LOG.error("", e);
